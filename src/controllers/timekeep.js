189ffe44e4e52a29693d0c977634f097b2d1cf72
Akreson <gimules.but@gmail.com>
2021-03-18 10:09:37 +0200

    Setup controller for /reports route

--- src/controllers/timekeep.js
+++ src/controllers/timekeep.js
@@ -26,6 +26,29 @@ const isValidStrParam = param => {
 
   return true;
 }
+
+const parseParamPassDate = checkDate => {
+  const [day, month, year] = checkDate.split(".").map(item => Number(item));
+
+  if ((day === undefined) || (day === NaN) || ((day < 1) || (day > 31))) return null;
+  if ((month === undefined) || (month === NaN) || ((month < 1) || (month > 12))) return null;
+  if ((year === undefined) || (year === NaN) || (year < 1970)) return null;
+
+  let date = new Date(year, month - 1, day);
+  return date
+}
+
+const convertPassDate = date => {
+  let result = null;
+  if (date !== undefined) {
+    result = parseParamPassDate(passDate);
+  } else {
+    result = DateUtils.getNowDate();
+  }
+
+  return result;
+}
+
 // TODO: extend error output
 
 exports.getUserDepartmentsAccessList = asynchandler(async (req, res, next) => {
@@ -43,39 +66,22 @@ exports.getUserDepartmentsAccessList = asynchandler(async (req, res, next) => {
   res.json(result);
 });
 
-const parseDivisionStatsDate = checkDate => {
-  const [day, month, year] = checkDate.split(".").map(item => Number(item));
-
-  if ((day === undefined) || (day === NaN) || ((day < 1) || (day > 31))) return null;
-  if ((month === undefined) || (month === NaN) || ((month < 1) || (month > 12))) return null;
-  if ((year === undefined) || (year === NaN) || (year < 1970)) return null;
-
-  let date = new Date(year, month - 1, day);
-  return date
-}
-
 exports.getDvisionTimekeepInfo = asynchandler(async (req, res, next) => {
   const divisionID = req.params.divisionID;
   const passDate = req.params.date;
   
   if (!isValidStrParam(divisionID)) {
-    return next(new ErrorResponse("Неуказано подразделение для запроса ", 400, "ValidationError"));
+    return next(new ErrorResponse("Неуказано отдел для запроса ", 400, "ValidationError"));
   }
   
   // TODO: Only for division id
   if (divisionID.search(/^\d+$/) === -1) {
-    return next(new ErrorResponse("Неправильно задан идентификатор подразделения", 400, "ValidationError"));
+    return next(new ErrorResponse("Неправильно задан идентификатор отдела", 400, "ValidationError"));
   }
   
-  let date = null;
-  if (passDate !== undefined) {
-    date = parseDivisionStatsDate(passDate);
-
-    if (date === null) {
-      return next(new ErrorResponse("Неверно заданая дата", 400, "ValidationError"));
-    }
-  } else {
-    date = DateUtils.getNowDate();
+  let date = convertPassDate(passDate);
+  if (date === null) {
+    return next(new ErrorResponse("Неверно заданая дата", 400, "ValidationError"));
   }
 
   const response = await processGetDivisionTimekeepStat(Number(divisionID), date);
@@ -95,15 +101,9 @@ exports.getUserTimekeepLog = asynchandler(async (req, res, next) => {
     return next(new ErrorResponse("Не указан поисковый LDAP логин", 400, "ValidationError"));
   }
 
-  let date = null;
-  if (passDate !== undefined) {
-    date = parseDivisionStatsDate(passDate);
-
-    if (date === null) {
-      return next(new ErrorResponse("Неверно заданая дата", 400, "ValidationError"));
-    }
-  } else {
-    date = DateUtils.getNowDate();
+  let date = convertPassDate(passDate);
+  if (date === null) {
+    return next(new ErrorResponse("Неверно заданая дата", 400, "ValidationError"));
   }
 
   const response = await processGetUserTimekeepLog(ldapName, date);
@@ -115,6 +115,13 @@ exports.getUserTimekeepLog = asynchandler(async (req, res, next) => {
   res.json(result);
 });
 
+// date: {
+//   low: date,
+//   high: date,
+//   type: str,
+//   departs: [ids...],
+// }
+
 exports.getDivisionsReports = asynchandler(async (req, res, next) => {
   const reqData = req.body["data"];
 
@@ -122,4 +129,35 @@ exports.getDivisionsReports = asynchandler(async (req, res, next) => {
     return next(new ErrorResponse("Неверно указан тип отчета", 400, "ValidationError"));
   }
 
+  let lowDate = convertPassDate(reqData.low);
+  if (lowDate === null) {
+    return next(new ErrorResponse("Неверно заданая начальная дата", 400, "ValidationError"));
+  }
+
+  let highDate = convertPassDate(reqData.high);
+  if (highDate === null) {
+    return next(new ErrorResponse("Неверно заданая конечная дата", 400, "ValidationError"));
+  }
+
+  if ((reqDate.departs !== undefined) && reqDate.departs.length) {
+    const InvalidDepartId = reqDate.departs.findIndex(id => {
+      if (typeof id === "number") return false;
+      return true;
+    });
+
+    if (InvalidDepartId !== -1) {
+      return next(new ErrorResponse(`Неверно указан идентификатор отдела под индексом ${InvalidDepartId}`, 400, "ValidationError"));  
+    }
+  } else {
+    return next(new ErrorResponse("Неуказаны отделы для отчета", 400, "ValidationError"));
+  }
+
+  const response = processGetDivisionsReports(departs, type, lowDate, highDate);
+  
+  if (!response) {
+    return next(new ErrorResponse("Данных не найдено", 400, "ValidationError"));
+  }
+
+  const result = succsesResponse(response);
+  res.json(result);
 });
