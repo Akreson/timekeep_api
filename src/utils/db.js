3cadbdae709d5beed5a28962ae4f6225a74dc32c
Akreson <gimules.but@gmail.com>
2021-04-29 10:06:04 +0300

    Update db.js file

--- src/utils/db.js
+++ src/utils/db.js
@@ -11,6 +11,7 @@ class ListElement {
   constructor(elem = null) {
     this.index = GLOBAL_COUNTER++; // NOTE: for debug
 
+    this.cb = null;
     this.data = elem;
     this.next = null;
     this.prev = null;
@@ -19,6 +20,7 @@ class ListElement {
 
 class List {
   constructor(elem) {
+    this.itemCount = 0;
     this.sentinel = new ListElement();
     this.sentinel.next = this.sentinel;
     this.sentinel.prev = this.sentinel;
@@ -53,6 +55,15 @@ class List {
     return elem;
   }
 
+  remove(elem) {
+    if (elem != null) {
+      elem.next.prev = elem.prev;
+      elem.prev.next = elem.next;
+      elem.next = null;
+      elem.prev = null;
+    }
+  }
+
   getbyData(data) {
     let resultItem = null;
 
@@ -75,16 +86,21 @@ class List {
 }
 
 class DbConnection {
-  constructor(options, poolSize = 1) {
+  constructor(options, poolSize = 10) {
+    this.connOptions = options;
+    this.poolSize = poolSize;
     this.freeConnList = new List();
     this.connList = new List();
     GLOBAL_COUNTER = 0;
+    this.connUsedCount = 0;
+  }
 
-    for (let i = 0; i < poolSize; ++i) {
-      let conn = mysql2.createConnection(options).promise();
-      const connElem = new ListElement(conn);
-      this.freeConnList.insert(connElem);
-    }
+  _createNewElem() {
+    let newConn = mysql2.createConnection(this.connOptions).promise();
+    const newConnElem = new ListElement(newConn);
+
+    ++this.connUsedCount;
+    return newConnElem;
   }
 
   async _getFreeConn() {
@@ -92,12 +108,21 @@ class DbConnection {
     
     while (true) {
       const connElem = this.freeConnList.getFirst();
-      
+    
       if (connElem === null) {
-        await setImmediatePromise();
+        if (this.connUsedCount == this.poolSize) {
+          await setImmediatePromise();
+        } else {
+          let newConnElem = this._createNewElem()
+          this.connList.insert(newConnElem);
+      
+          resultConn = newConnElem.data;
+          break;
+        }
       } else {
-        resultConn = connElem.data;
+        clearTimeout(connElem.cb);
         this.connList.insert(connElem);
+        resultConn = connElem.data;
         break;
       }
     }
@@ -105,8 +130,19 @@ class DbConnection {
     return resultConn;
   }
 
+  _releaseConn(connElem) {
+    this.freeConnList.remove(connElem)
+    connElem.data.end();
+    connElem = null;
+    
+    --this.connUsedCount;
+  }
+
   _putConnToFreeList(conn) {
+    const releaseTime = 5 * DateUtils.millisecInSec;
+
     let connElem = this.connList.getbyData(conn);
+    connElem.cb = setTimeout(this._releaseConn.bind(this, connElem), releaseTime);
     this.freeConnList.insert(connElem);
   }
 
